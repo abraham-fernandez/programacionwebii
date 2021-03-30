ba8909e11adcdf59a9a2c9d52d4a613634e618d3
abraham.fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2021-03-30 13:06:15 +0200

    puesta en marcha, primeros resolvers

--- demas_entregas/services/stats_server/server.js
+++ demas_entregas/services/stats_server/server.js
@@ -1,1 +1,59 @@
-// TBI
+const { GraphQLServer } = require("graphql-yoga");
+const fetch = require("node-fetch");
+
+const fetchJson = (...args) => fetch(...args).then(response => response.json());
+
+const typeDefs = `
+    type Query {
+        pair(key:ID!):Pair,
+        pairs: [Pair]
+    }
+    type Mutation {
+        createStat(player:String!,gameScore:Int!) : Stat!
+    }
+    type Pair {  
+        key:ID,
+        applicationId:String,
+        value: Stat
+    }
+    type Stat{            
+         player:String,
+         gameScore:Int
+    }
+`;
+
+const BASE_URL = "https://0qh9zi3q9g.execute-api.eu-west-1.amazonaws.com/development";
+const config={method:"GET",headers:{"x-application-id":"abraham.fernandez"}}
+const resolvers = {
+    Query: {
+        pair: (_, {key}) =>{ config.method="GET"; return fetchJson(`${BASE_URL}/pairs/${key}/`,config)},
+        pairs: (_,) =>{ config.method="GET"; return fetchJson(`${BASE_URL}/pairs/`,config)},
+    },
+    Mutation: {
+        createStat: (_, args) => {
+            config.method="PUT"
+            config.body=JSON.stringify({player:args.player,gameScore:args.gameScore})
+
+            return fetchJson(`${BASE_URL}/pairs/`+'99a694cc-74ba-46ec-bd55-a893f6a9c8fb',config)
+
+
+        }
+    },
+    Stat:{
+        player:stat=>JSON.parse(stat).player,
+        gameScore:stat=>JSON.parse(stat).gameScore,
+    }
+}
+
+
+
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start({
+    playground: "/",
+    port: 3000
+});
+
+
+
