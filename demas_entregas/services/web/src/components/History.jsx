2863d6d3bd0dfaf13c54488f1978778a19debd6e
abraham.fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2021-04-07 13:55:00 +0200

    estructura estadisticas

--- demas_entregas/services/web/src/components/History.jsx
+++ demas_entregas/services/web/src/components/History.jsx
@@ -5,6 +5,21 @@ import ProgressBar from "./StatsBar";
 
 const History = () => {
     const {user} = useContext(AuthContext);
+    const mdColors = [
+        '#F44336',
+        '#1565C0',
+        '#FFCDD2',
+        '#66BB6A',
+        '#E57373',
+        '#827717',
+        '#F44336',
+        '#E53935',
+        '#D32F2F',
+        '#EC407A',
+        '#E91E63',
+        '#D81B60',
+        '#C2185B',
+        '#AD1457']
 
     const [stats, setStats] = useState([])
 
@@ -32,7 +47,7 @@ const History = () => {
                 let max = listItems[0].value.gameScore
                 listItems.map(e => e.value.percentage =( e.value.gameScore / max) * 100)
                console.log(listItems)
-                setStats(listItems.map((e,idx)=> <ProgressBar key={idx} bgcolor="#6a1b9a" player={e.value.player} score={e.value.gameScore} completed={e.value.percentage} />))
+                setStats(listItems.map((e,idx)=> <ProgressBar key={idx} bgcolor={mdColors[idx]} player={e.value.player} score={e.value.gameScore} completed={e.value.percentage} />))
             });
 
     }
